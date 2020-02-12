ca5ff5105c209cd448099ba42b6c1483c849a2cf
blacksheeptwin <60577833+blacksheeptwin@users.noreply.github.com>
2020-02-12 13:05:07 -0500

    Added Print Line

---
+++ test.py
@@ -0,0 +1,11 @@
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+    for line in fh:
+        line = line.rstrip()
+        line= line.upper()
+        print (line)
+        print (line)
+except:
+    print('Enter a valid file name:')
+    exit()
