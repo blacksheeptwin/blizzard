4ab4286bf09eda9e0a9a3220c098e4a51cc05ef1
blacksheeptwin <60577833+blacksheeptwin@users.noreply.github.com>
2020-02-12 21:13:34 -0500

    updated to new program to search file

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-fname = input("Enter file name: ")
-try:
-    fh = open(fname)
-    for line in fh:
-        line = line.rstrip()
-        line= line.upper()
-        print (line)
-
-except:
-    print('Enter a valid file name:')
-    exit()
-#trial again
-#trying
+file = input("Enter file name: ")
+fh = open(file)
+count = 0
+total = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count = count + 1
+        line2 = line[20:]
+        line3 = float(line2)
+        total = total+line3
+average = total/count
+print("Average spam confidence:",average)
