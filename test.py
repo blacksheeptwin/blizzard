344783c6041391a2ccf8a13432e19927164d2649
blacksheeptwin <60577833+blacksheeptwin@users.noreply.github.com>
2020-02-13 11:28:16 -0500

    Added in code for assignment 8.4

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 file = input("Enter file name: ")
 fh = open(file)
 count = 0
@@ -10,3 +11,16 @@ for line in fh:
         total = total+line3
 average = total/count
 print("Average spam confidence:",average)
+'''
+#Assignment 8.4
+list2 = list()
+file = input ("Enter file name: ")
+fh = open(file)
+
+for liam in fh:
+    new_list =liam.split()
+    for words in new_list:
+        if (words not in list2):
+            list2.append(words)
+list2.sort()
+print(list2)
